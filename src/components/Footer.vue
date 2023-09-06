bc5a89335cb63dfcf924bf149b3da8ca8920e298
fowardslash <68984861+fowardslash@users.noreply.github.com>
2023-09-06 17:49:08 +0700

    revamped footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,54 +1,102 @@
+<script>
+import { RouterLink } from 'vue-router';
+import { getFeaturedProjects, getAllData, getProjects } from '../../database/helper';
+import { getUsersWithId } from '../../database/helper';
+
+    export default{
+    data() {
+        return {
+            list: getAllData().then(e => this.list = e),
+            usrs: getUsersWithId().then(e => this.usrs = e)
+        };
+    },
+    components: { RouterLink }
+}
+</script>
 <template>
     <div class="footer">
         <div class="about_section">
-            <p class="bold">Copyright 2023 Quang, the design is made by me.</p>
-            <p>This project is Open Source. Please see its project page <router-link to="/projects/0gV2wAXOaCq89vnZ5vHG">Here</router-link>.</p>
+            <div>
+                <img src="/logo.svg"/>
+                <p>Just a Website created by a college student</p>
+               
+            </div>
             <div class="socials">
-                <a href="https://www.facebook.com/wuang.96/" class="ri-facebook-circle-fill social"></a>
-                <a href="https://twitter.com/quangrain69" class="ri-twitter-fill social"></a>
-                <a href="https://www.linkedin.com/in/quang-tr%C3%AD-5127b5222/" class="ri-linkedin-fill social"></a>
+                <a href="https://github.com/fowardslash"><i class="ri-github-fill"></i></a>
+                <a href="https://twitter.com/quangrain69"><i class="ri-twitter-fill"></i></a>
+                <a href="mailto://quangrain2014@gmail.com"><i class="ri-mail-fill"></i></a>
             </div>
         </div>
-        
-        <div class="email_section">
-            <i class="ri-mail-line"></i>
-            <p><b>Email</b><br/>quangrain2014@gmail.com</p>
-            
+        <div class="bottom_section">
+            <div class="links_section" id="one">
+                <h3>Projects</h3>
+                <div class="links">
+                    <RouterLink v-for="i in list" :to="`/projects/${i.id}`">{{ i.name }}</RouterLink>
+                </div>
+            </div>
+            <div class="links_section" id="two">
+                <h3>Contributors</h3>
+                <div class="links">
+                    <a v-for="i in usrs" :href="i.github">{{ i.fullname }}</a>
+                </div>
+            </div>
+            <div class="links_section" id="three">
+                <h3>Links</h3>
+                <RouterLink to="/about">About</RouterLink>
+                <RouterLink to="/credits">Credits</RouterLink>
+                <RouterLink to="/project">Projects</RouterLink>
+            </div>
         </div>
-        <router-link to="/about" class="generic_button">
-            <p>About me</p>
-        </router-link>
-        <router-link to="/project" class="generic_button">
-            <p>Projects</p>
-        </router-link>
-        <router-link to="/blog" class="generic_button">
-            <p>Blog</p>
-        </router-link>
-        <router-link to="/credits" class="generic_button">
-            <p>Credits</p>
-        </router-link>
-        <img src="/logo.svg"/>
-
+        <div class="links_section">
+            <p>Copyright 2023 Quang, This project and its design is created and owned by Quang. Icons used are from <a href="https://remixicon.com/">Remix Icon</a>, database and cloud storage services are provided by <a href="https://firebase.google.com/">Firebase</a>. All logos used belong to its owners.</p>
+        </div>
     </div>
 </template>
 <style scoped>
     .footer{
         display: flex;
-        justify-content: space-between;
-        align-items: center;
+        flex-direction: column;
+        gap: 25px;
         border-radius: 0px;
         padding: 25px;
         background-color: var(--background);
-        border-top: 2px var(--foreground) solid;
+        border-top: 5px var(--text) solid;
+    }
+    #one{
+        border-top: 5px var(--primary) solid;
+    }
+    #two{
+        border-top: 5px var(--secondary) solid;
+    }
+    #three{
+        flex: 0;
+    }
+    .links_section{
+        padding: 15px;
+        border-top: var(--text) 5px solid;
+        border-radius: 0;
+        flex: 1;
+        display: flex;
+        gap: 15px;
+        flex-direction: column;
+    }
+    .links{
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        gap: inherit;
+    }
+    .links_section a{
+        color: var(--text);
+        text-decoration-thickness: 2px;
     }
     .about_section{
         display: flex;
-        flex-direction: column;
+        justify-content: space-between;
         gap: 0px;
     }
     .footer img{
         width: max-content;
-        height: 24px;
+        height: 32px;
     }
     .email_section{
         display: flex;
@@ -72,8 +120,18 @@
         gap: 10px;
         align-items: center;
     }
+    .socials i{
+        font-size: 32px;
+    }
+    .socials a{
+        text-decoration: none;
+    }
     .generic_button{
         outline: none !important
+    }
+    .bottom_section{
+        display: flex;
+        gap: 15px;
     }
     @media only screen and (max-width: 600px) {
         .footer{
